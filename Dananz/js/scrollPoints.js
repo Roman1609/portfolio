5664305381ea5514dc3e9b750810977343a34a70
Roman1609 <109855271+Roman1609@users.noreply.github.com>
2024-02-08 14:56:49 +0200

    Add files via upload

---
+++ Dananz/js/scrollPoints.js
@@ -0,0 +1,63 @@
+"use strict";
+
+// Chat GPT Code ===========================
+
+const menuScrollPoints = document.querySelectorAll('[data-scroll-to]');
+const toScrollBlocks = document.querySelectorAll('[data-to-scroll]');
+ // Визначте бажане значення зміщення
+/*
+menuScrollPoints.forEach((menuScrollPoint) => {
+    const dataPoint = menuScrollPoint.dataset.scrollTo;
+    toScrollBlocks.forEach((toScrollBlock) => {
+        const toBlock = toScrollBlock.dataset.toScroll;
+        if (dataPoint == toBlock) {
+            menuScrollPoint.addEventListener("click", (event) => {
+                event.preventDefault(); // Зупинити стандартну обробку кліку
+
+                const targetPosition = toScrollBlock.getBoundingClientRect().top + window.scrollY;
+                const scrollToPosition = targetPosition - offsetValue;
+
+                window.scrollTo({
+                    top: scrollToPosition,
+                    behavior: "smooth"
+                });
+
+                bodyElement.classList.remove('menu-open');
+            });
+        }
+    });
+});
+*/
+
+/*
+Відредаговано, взяти padding-top від кожного блоку =====
+!!! Протестувати !!!
+*/
+
+menuScrollPoints.forEach((menuScrollPoint) => {
+    const dataPoint = menuScrollPoint.dataset.scrollTo;
+    toScrollBlocks.forEach((toScrollBlock) => {
+        const toBlock = toScrollBlock.dataset.toScroll;
+        if (dataPoint == toBlock) {
+            menuScrollPoint.addEventListener("click", (e) => {
+                e.preventDefault(); // Зупинити стандартну обробку кліку
+
+                // Отримати значення padding-top від toScrollBlock
+                const computedStyle = window.getComputedStyle(toScrollBlock);
+                const paddingTop = parseFloat(computedStyle.paddingTop);
+
+                // Використовуйте paddingTop для обчислення нового значення зміщення
+                const targetPosition = toScrollBlock.getBoundingClientRect().top + window.scrollY;
+                const scrollToPosition = targetPosition - paddingTop;
+
+                window.scrollTo({
+                    top: scrollToPosition,
+                    behavior: "smooth"
+                });
+                burgerButton.classList.remove('burger-active');
+                bodyElement.classList.remove('menu-open');
+            });
+        }
+    });
+});
+
